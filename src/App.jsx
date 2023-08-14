f238f93b8e420932eaede6906e23b897a79ede00
bradleymatjie <bradleymatjie.job@gmail.com>
2023-08-14 19:38:06 +0200

    home

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import './App.css';
-import { Homepage } from './Components/Homepage/Homepage';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './Pages/Home/Home'
+import { Nav } from './Components/Nav/Nav'
+import { Bag } from './Components/Bag/Bag';
 
 function App() {
+  const [menuToggler, setMenuToggler] = useState(false);
+
   return (
-    <>
-      <Homepage/>
-    </>
-  );
+    <div className='app'>
+      <Nav menuTogglerFunction={setMenuToggler} 
+        menuTogglerValue={menuToggler} />
+      <Routes>
+        <Route path='/store/' element={<Home />}/>
+      </Routes>
+      <Bag />
+    </div>
+  )
 }
 
-export default App;
-
+export default App
